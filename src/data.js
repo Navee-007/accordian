d70c82d94a95c14a1c92b5e24aeadcc51202c4f9
dina <naveenkumar.coder@gmail.com>
2023-03-29 12:17:40 +0530

    update

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'title-1',
+    info:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+  },
+    {
+        id: 2,
+        title: 'title-2',
+        info:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+    },
+  {
+    id: 3,
+    title: 'title-3',
+    info:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+  },
+  {
+    id: 4,
+     title: 'title-4',
+    info:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+  },
+  {
+    id: 5,
+    title: 'title-5',
+    info:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+  },
+]
+export default questions
